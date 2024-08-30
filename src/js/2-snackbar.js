aee8d1762b1841a99f4321dbf13d47e38ab8b4de
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-30 16:34:30 +0300

    add

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -23,7 +23,7 @@ function enteredTime(event) {
       backgroundColor: '#59A10D',
       position: 'center',
       title: ' OK',
-      iconUrl: 'img/Group.svg',
+      iconUrl: './img/Group.svg',
       message: `✅ Fulfilled promise in ${time} ms`,
     });
   });
@@ -32,7 +32,7 @@ function enteredTime(event) {
       backgroundColor: '#EF4040',
       position: 'center',
       title: 'Error',
-      iconUrl: 'img/Group-X.svg',
+      iconUrl: './img/Group-X.svg',
       message: `❌ Rejected promise in ${time} ms`,
     });
   });
