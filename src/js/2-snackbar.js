f17df953b8ae0b4f8d25d3c1e2f26d85f071e484
Galushko Evgeniy <yudginchik@gmail.com>
2024-08-30 16:24:49 +0300

    add task - 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,43 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+function enteredTime(event) {
+  event.preventDefault();
+  const data = event.target;
+  const time = data.elements.delay.value;
+  const promiseState = data.elements.state.value;
+  console.log({ time: `${time}`, radio: `${promiseState}` });
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (promiseState === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, time);
+  });
+  promise.then(value => {
+    iziToast.warning({
+      backgroundColor: '#59A10D',
+      position: 'center',
+      title: ' OK',
+      iconUrl: '../img/Group.svg',
+      message: `✅ Fulfilled promise in ${time} ms`,
+    });
+  });
+  promise.catch(error => {
+    iziToast.warning({
+      backgroundColor: '#EF4040',
+      position: 'center',
+      title: 'Error',
+      iconUrl: '../img/Group-X.svg',
+      message: `❌ Rejected promise in ${time} ms`,
+    });
+  });
+  console.log(promise);
+  form.reset();
+}
+
+form.addEventListener('submit', enteredTime);
